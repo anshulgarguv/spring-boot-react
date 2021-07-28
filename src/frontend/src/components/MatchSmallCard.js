7ff3c7f3698948369ce8562f102feb0120444b3e
Anshul <Anshul.Garg@team.telstra.com>
2021-07-28 15:28:26 +0530

    Styled Team page

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,12 +1,16 @@
 import {React} from 'react';
 import {Link} from 'react-router-dom';
+import './MatchSmallCard.scss';
 export const MatchSmallCard = ({match,teamName}) =>{
   const otherTeamName = teamName === match.team1 ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeamName}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className="MatchSmallCard">
-      <h3>vs <Link to={otherTeamRoute}>{otherTeamName}</Link> </h3>
-      <p>{match.matchWinner} won by {match.resultMargin} {match.result} </p>
+    <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
+      <span className="vs">vs</span>
+      <h1><Link to={otherTeamRoute}>{otherTeamName}</Link></h1>
+        <p className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result} </p>
+        
     </div>
   );
 }
